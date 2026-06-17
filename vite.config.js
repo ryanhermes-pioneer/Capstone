61b3b2d8e598136484f7bae58306cfb9949a5455
ryanhermes-pioneer <ryan.hermes@thepioneerteam.com>
2026-06-17 13:40:47 -0500

    fix path

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  root: 'Close Tracker',
-  envDir: process.cwd(),
+  root: join(__dirname, 'Close Tracker'),
+  envDir: __dirname,
 });
